8a2a8d1db086d3da822f7f6f2f117e46405e409a
hiroki-tategami <qazxcbmn609@gmail.com>
2023-09-12 11:20:08 +0900

    appusercontrollerの編集

--- Etc/Laravel/AppUserController.php
+++ Etc/Laravel/AppUserController.php
@@ -6,58 +6,12 @@ use App\Models\AppUser;
 use Illuminate\Http\Request;
 
 class AppUserController extends Controller
-{
+{    
     public function app_user($id)
     {
         $app_user = AppUser::find($id);
         return view('app_user', ['app_user' => $app_user]);
     }
-
-    public function play_chara_gacha($id)
-    {
-        $app_user = AppUser::find($id);
-        $has_chara_flag = $app_user->has_chara_flag;
-        $result_string = "";
-
-        $rand_num = mt_rand(1, 10000);
-        $jouyo = $rand_num % 32;
-        $result_string = $result_string.$jouyo.",";
-        $jouyo_flag = 1 << $jouyo;
-        $app_user->has_chara_flag = $has_chara_flag | $jouyo_flag;
-        $app_user->save();
-
-        return view('play_chara_gacha', ['result_string' => $result_string]);
-    }
-
-    public function play_chara_gacha_10($id)
-    {
-        $app_user = AppUser::find($id);
-        $has_chara_flag = $app_user->has_chara_flag;
-        $result_string = "";
-
-        $gachaCount = 10;
-        for ($i = 0; $i < $gachaCount; $i++) {
-            $rand_num = mt_rand(1, 10000);
-            $jouyo = $rand_num % 32;
-            $result_string = $result_string.$jouyo.",";
-            $jouyo_flag = 1 << $jouyo;
-            $has_chara_flag = $has_chara_flag | $jouyo_flag;
-        }
-        $app_user->has_chara_flag = $has_chara_flag;
-        $app_user->save();
-
-        return view('play_chara_gacha', ['result_string' => $result_string]);
-    }
-
-    public function clear_has_chara_flag($id)
-    {
-        $app_user = AppUser::find($id);
-        $app_user->has_chara_flag = 0;
-        $app_user->save();
-
-        return view('nothing');
-    }
-
 
     /**
      * Display a listing of the resource.
